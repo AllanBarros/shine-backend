94af8954a9f98373b68b89e9b6d081c8d773e784
Allan <Allanbmalves@gmail.com>
2022-10-16 15:07:54 -0300

    Remoção de comentários

--- routers/v1/event_router.py
+++ routers/v1/event_router.py
@@ -9,21 +9,6 @@ EventRouter = APIRouter(
     prefix="/v1/eventos", tags=["eventos"]
 )
 
-# @EventRouter.get("/detalhes", response_model=List[EventSchema])
-# def index(
-#     name: Optional[str] = None,
-#     pageSize: Optional[int] = 100,
-#     startIndex: Optional[int] = 0,
-#     authorService: AuthorService = Depends(),
-# ):
-#     return [
-#         author.normalize()
-#         for author in authorService.list(
-#             name, pageSize, startIndex
-#         )
-#     ]
-
-
 @EventRouter.get("/detalhes/{id}", response_model=EventSchema)
 def get(id: int, eventService: EventService = Depends()):
     return eventService.get_event_detail(id)
